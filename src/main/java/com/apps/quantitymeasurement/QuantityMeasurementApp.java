01117d759fbba3f7b4418646feabeb69da4f539b
Ananya Shukla <ananyashukla990@gmail.com>
2026-02-22 20:31:54 +0530

    [Ananya] : Added Quantity measurement app

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,31 +2,22 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-	// Static method for comparing feet
-	public static boolean areFeetEqual(double value1, double value2) {
-		Feet f1 = new Feet(value1);
-		Feet f2 = new Feet(value2);
-		return f1.equals(f2);
-	}
-
-	// Static method for comparing inches
-	public static boolean areInchesEqual(double value1, double value2) {
-		Inches i1 = new Inches(value1);
-		Inches i2 = new Inches(value2);
-		return i1.equals(i2);
-	}
-
 	public static void main(String[] args) {
 
-		// Comparing two feet value
-		boolean feetResult = areFeetEqual(1.0, 1.0);
-		System.out.println("Input values : 1.0 ft and 1.0 ft");
-		System.out.println("Output: Equal " + feetResult );
+        // Cross-unit comparison
+		QuantityLength l1 = new QuantityLength(1.0, LengthUnit.FEET);
+		QuantityLength l2 = new QuantityLength(12.0, LengthUnit.INCH);
 
-		// Comparing two Inches value
-		boolean inchResult = areInchesEqual(1.0, 1.0);
-		System.out.println("Input values : 1.0 inch and 1.0 inch");
-		System.out.println("Output: Equal " + inchResult );
+		// Results of comparison
+        System.out.println("Input values : " + l1 + " and " + l2);
+        System.out.println("Output: Equal " + l1.equals(l2) );
 
-	}
+        // Same-unit comparison
+        QuantityLength l3 = new QuantityLength(1.0, LengthUnit.INCH);
+        QuantityLength l4 = new QuantityLength(1.0, LengthUnit.INCH);
+
+        // Results of comparison
+        System.out.println("Input values: " + l3 + " and " + l4);
+        System.out.println("Output: Equal " + l3.equals(l4) );
+    }
 }
