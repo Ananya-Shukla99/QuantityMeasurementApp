81002c6d20975bbb1c3510aecde2af770931432c
Ananya Shukla <ananyashukla990@gmail.com>
2026-02-22 22:54:30 +0530

    [Ananya] : Added modular comparison method and extended demo cases for yards and centimeters

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -1,20 +1,19 @@
 package com.apps.quantitymeasurement;
 
 public enum LengthUnit {
-	
-	// Base unit :Feet, Inches convert to feet 
-	FEET(1.0), INCH(1.0 / 12.0);
-	
+
+	INCH(1.0), FEET(12.0), YARDS(36.0), CENTIMETERS(0.393701);
+
 	// Conversion factor
 	private final double conversionFactor;
 
-	//Constructor
-    LengthUnit(double conversionFactor) {
-        this.conversionFactor = conversionFactor;
-    }
+	// Constructor
+	LengthUnit(double conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
 
-    // Converting to base unit
-    public double getConversionFactor() {
-        return conversionFactor;
-    }
+	// Converting to base unit
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
 }
