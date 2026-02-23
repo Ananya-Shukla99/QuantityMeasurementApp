84c3172be142bbd98f0e3ab1772d96b91eb88278
Ananya Shukla <ananyashukla990@gmail.com>
2026-02-23 16:52:58 +0530

    [Ananya] : Added static method for implementing feature of addition of Two Length Units with target type

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,46 +2,69 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-	// Common display utility method
-	private static void displayResult(String operation, Object result) {
-		System.out.println(operation + " : " + result);
+	// Static equality demonstration
+	public static boolean demonstrateLengthEquality(QuantityLength l1, QuantityLength l2) {
+		return l1.equals(l2);
 	}
 
-	// Static conversion demonstration
-	public static double demonstrateLengthConversion(double value, LengthUnit from, LengthUnit to) {
+	// Private helper for printing equality
+	private static void checkEquality(QuantityLength l1, QuantityLength l2) {
 
-		double result = QuantityLength.convert(value, from, to);
-
-		displayResult("convert(" + value + ", " + from + ", " + to + ")", result);
-
-		return result;
+		System.out.println(l1 + " and " + l2 + " Equal: " + demonstrateLengthEquality(l1, l2));
 	}
 
-	// Instance conversion demonstration (Overloaded method)
-	public static QuantityLength demonstrateLengthConversion(QuantityLength length, LengthUnit to) {
 
-		if (length == null) {
-			throw new IllegalArgumentException("Length object cannot be null");
-		}
+	// Static method for length conversion 
+	public static void demonstrateLengthConversion(double value, LengthUnit fromUnit, LengthUnit toUnit) {
 
-		QuantityLength result = length.convertTo(to);
+		double result = LengthUnit.valueOf(fromUnit.name()).convertFromBaseUnit(fromUnit.convertToBaseUnit(value));
 
-		displayResult(length + " converted to " + to, result);
+		System.out.println(value + " " + fromUnit + " is " + result + " " + toUnit);
+	}
 
-		return result;
+	// Object-based conversion
+	public static void demonstrateLengthConversion(QuantityLength length, LengthUnit toUnit) {
+
+		System.out.println(length + " converted to " + toUnit + " is " + length.convertTo(toUnit));
+	}
+
+	// Static method for length addition 
+	public static void demonstrateLengthAddition(double value1, LengthUnit unit1, double value2, LengthUnit unit2) {
+
+		QuantityLength length1 = new QuantityLength(value1, unit1);
+		QuantityLength length2 = new QuantityLength(value2, unit2);
+
+		QuantityLength result = length1.add(length2, LengthUnit.FEET);
+
+		System.out.println(length1 + " + " + length2 + " = " + result);
+	}
+
+	// Static method for length addition with target type
+	public static void demonstrateLengthAddition(double value1, LengthUnit unit1, double value2, LengthUnit unit2,
+			LengthUnit targetUnit) {
+
+		QuantityLength length1 = new QuantityLength(value1, unit1);
+		QuantityLength length2 = new QuantityLength(value2, unit2);
+
+		QuantityLength result = length1.add(length2, targetUnit);
+
+		System.out.println(length1 + " + " + length2 + " = " + result);
 	}
 
 	public static void main(String[] args) {
 
-		// Conversion demonstrations
+		// Equality method 
+		checkEquality(new QuantityLength(1.0, LengthUnit.FEET), new QuantityLength(12.0, LengthUnit.INCH));
+
+		// Conversion method 
 		demonstrateLengthConversion(1.0, LengthUnit.FEET, LengthUnit.INCH);
-		demonstrateLengthConversion(3.0, LengthUnit.YARDS, LengthUnit.FEET);
-		demonstrateLengthConversion(36.0, LengthUnit.INCH, LengthUnit.YARDS);
-		demonstrateLengthConversion(1.0, LengthUnit.CENTIMETERS, LengthUnit.INCH);
 
-		// Object-based conversion demo
-		QuantityLength length = new QuantityLength(2.0, LengthUnit.YARDS);
-		demonstrateLengthConversion(length, LengthUnit.FEET);
+		demonstrateLengthConversion(new QuantityLength(2.0, LengthUnit.YARDS), LengthUnit.FEET);
 
+		// Addition
+		demonstrateLengthAddition(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCH);
+
+		// Addition with target unit
+		demonstrateLengthAddition(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCH, LengthUnit.YARDS);
 	}
 }
