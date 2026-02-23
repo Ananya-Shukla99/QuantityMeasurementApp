a1ba8d8b06e15b1d3b455ba57d5ce4f504bfcb7d
Ananya Shukla <ananyashukla990@gmail.com>
2026-02-23 16:50:35 +0530

    [Ananya] : Added new feature of addition of Two Length Units with target type

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -4,17 +4,24 @@ public enum LengthUnit {
 
 	INCH(1.0), FEET(12.0), YARDS(36.0), CENTIMETERS(0.393701);
 
-	// Conversion factor
 	private final double conversionFactor;
 
-	// Constructor
 	LengthUnit(double conversionFactor) {
 		this.conversionFactor = conversionFactor;
 	}
+	// Returns conversion factor relative to base unit
+		public double getConversionFactor() {
+			return conversionFactor;
+		}
 
-	// Converting to base unit
-	public double getConversionFactor() {
-		return conversionFactor;
-	}
+		// Convert given value to base unit 
+		public double convertToBaseUnit(double value) {
+			return value * conversionFactor;
+		}
+
+		// Convert from base unit to current unit
+		public double convertFromBaseUnit(double baseValue) {
+			return baseValue / conversionFactor;
+		}
 	
 }
