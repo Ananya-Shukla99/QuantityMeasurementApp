9cf4398bb0fe21c9d005006acf1390c4bc88f55a
Ananya Shukla <ananyashukla990@gmail.com>
2026-03-15 20:11:58 +0530

    [Ananya] :  Added ArithmeticOperation enum and SupportsArithmetic functional interface

---
+++ src/main/java/com/apps/quantitymeasurement/core/ArithmeticOperation.java
@@ -0,0 +1,6 @@
+package com.apps.quantitymeasurement.core;
+
+public enum ArithmeticOperation {
+
+	ADD, SUBTRACT, DIVIDE
+}
---
+++ src/main/java/com/apps/quantitymeasurement/core/SupportsArithmetic.java
@@ -0,0 +1,8 @@
+package com.apps.quantitymeasurement.core;
+
+@FunctionalInterface
+public interface SupportsArithmetic {
+
+	// single abstract method
+	boolean isSupported();
+}
